f95808bd0499137466f8555599e88f3cc6f1970a
shenxingbo <2461152330@qq.com>
2018-09-11 15:15:07 +0800

    完成轮播图

--- taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
+++ taotao-portal/src/main/java/com/taotao/portal/controller/IndexController.java
@@ -1,6 +1,9 @@
 package com.taotao.portal.controller;
 
+import com.taotao.portal.service.ContentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -11,9 +14,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
+
+    @Autowired
+    private ContentService service;
+
     @RequestMapping("/index")
-    public String showIndex() {
+    public String showIndex(Model model) {
+        String adJson = service.getContentList();
+        System.out.println( adJson);
+        model.addAttribute("ad1", adJson);
         return "index";
     }
+
 
 }
